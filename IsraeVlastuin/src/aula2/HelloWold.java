f403ef456de3e0545085da8483e002a0c0fbe009
Israelvlastuin <124810096+Israelvlastuin@users.noreply.github.com>
2024-02-09 17:25:41 -0300

    Add files via upload

---
+++ IsraeVlastuin/src/aula2/HelloWold.java
@@ -0,0 +1,10 @@
+package aula2;
+
+public class HelloWold {
+    public static void main(String[] args) {
+        String meuTexto = "Meu Texto!";
+        System.out.println(meuTexto); 
+        System.out.println("Hello World");
+    }
+    
+}
